5723580e659cda2d62778872283e63afd1a3351c
sujal_shrestha <sujal.scxi121@amv.edu.np>
2023-03-17 19:37:56 +0545

    completed 02-doubly-linkedlist

--- implementation/02-doubly-linked-list.js
+++ implementation/02-doubly-linked-list.js
@@ -1,70 +1,97 @@
 // Node class is implemented for you, no need to look for bugs here!
 class DoublyLinkedNode {
-    constructor(val) {
-        this.value = val;
-        this.next = null;
-        this.prev = null;
-    }
+  constructor(val) {
+    this.value = val;
+    this.next = null;
+    this.prev = null;
+  }
 }
 
 class DoublyLinkedList {
-    constructor() {
-        this.head = null;
-        this.tail = null;
-        this.length = 0;
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  addToHead(val) {
+    // There are bugs in this method! Fix them!!!
+    // Write your hypothesis on the time complexity of this method here
+
+    // Add node of val to head of linked list
+    let newNode = new DoublyLinkedNode(val);
+    let current = this.head;
+
+    if (current) {
+      current.prev = newNode;
+      newNode.next = current;
+      this.head = newNode;
+    } else {
+      this.head = newNode;
+      this.tail = newNode;
     }
 
-    addToHead(val) {
-        // There are bugs in this method! Fix them!!!
-        // Write your hypothesis on the time complexity of this method here
+    this.length++;
+  }
 
-        // Add node of val to head of linked list
-        let newNode = new DoublyLinkedNode(val);
+  addToTail(val) {
+    // Add node of val to tail of linked list
+    let newNode = new DoublyLinkedNode(val);
+    if (this.head) {
+      newNode.prev = this.tail;
+      this.tail.next = newNode;
+      this.tail = newNode;
+    } else {
+      this.head = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    // Write your hypothesis on the time complexity of this method here
+  }
 
-        if (this.length >= 0) {
-            this.head.previous = newNode;
-            newNode.next = this.head;
-            this.head = newNode;
-        } else {
-            this.head = newNode;
-            this.tail = newNode;
+  removeFromHead() {
+    // Remove node at head
+    if (this.head) {
+      let current = this.head;
+      this.head = this.head.next;
+      if (this.head) {
+        this.head.prev = null;
+      }
+      this.length--;
+      return current.value;
+    }
+    // Write your hypothesis on the time complexity of this method here
+  }
+
+  removeFromTail() {
+    // Remove node at tail
+    // Write your hypothesis on the time complexity of this method here
+    if(this.head){
+        let current = this.tail;
+        this.tail=this.tail.prev;
+        if(this.tail){
+            this.tail.next=null;
         }
-
-        this.length++;
-    }
-
-    addToTail(val) {
-        // Add node of val to tail of linked list
-
-        // Write your hypothesis on the time complexity of this method here
-    }
-
-    removeFromHead() {
-        // Remove node at head
-
-        // Write your hypothesis on the time complexity of this method here
-    }
-
-    removeFromTail() {
-        // Remove node at tail
-
-        // Write your hypothesis on the time complexity of this method here
-    }
-
-    peekAtHead() {
-        // Return value of head node
-
-        // Write your hypothesis on the time complexity of this method here
-    }
-
-    peekAtTail() {
-        // Return value of tail node
-        
-        // Write your hypothesis on the time complexity of this method here
+        this.length--;
+        return current.value;
     }
 }
+
+  peekAtHead() {
+    // Return value of head node
+    if (this.head) return this.head.value;
+    // Write your hypothesis on the time complexity of this method here
+  }
+
+  peekAtTail() {
+    // Return value of tail node
+    // Write your hypothesis on the time complexity of this method here
+        if(this.tail) return this.tail.value;
+    }
+}
+
 
 module.exports = {
-    DoublyLinkedList,
-    DoublyLinkedNode
-}
+  DoublyLinkedList,
+  DoublyLinkedNode,
+};
