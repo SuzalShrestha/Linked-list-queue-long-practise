8566dc16e5529ac0c2f8b3c5cde4c951f8895ea0
sujal_shrestha <sujal.scxi121@amv.edu.np>
2023-03-17 19:49:11 +0545

    added 03-queue

--- implementation/03-queue.js
+++ implementation/03-queue.js
@@ -5,16 +5,39 @@ class Queue {
     constructor() {
         this.head = null;
         this.tail = null;
+        this.length=0;
     }
 
     enqueue(val) {
         // Add node to end of queue (linked list)
-
+        let newNode=new SinglyLinkedNode(val);
+        if(this.head){
+            this.tail.next=newNode;
+            this.tail=newNode;
+        }else{
+            this.head=newNode;
+            this.tail=newNode;
+        }
+        this.length++;
+        return this.length;
         // Write your hypothesis on the time complexity of this method here
     }
 
     dequeue() {
         // Remove node from front of queue (linked list)
+        if(this.length===1){
+            let current=this.head;
+            this.head=null;
+            this.tail=null;
+            return current.value;
+        }
+        if(this.head){
+            let temp=this.head;
+            this.head=this.head.next;
+            this.length--;
+            return temp.value;
+        }
+        return null;
         
         // Write your hypothesis on the time complexity of this method here
     }
